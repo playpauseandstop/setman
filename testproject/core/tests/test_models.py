5c330b4911405a483a24f2d45b688104dba844e8
Igor Davydenko <playpauseandstop@gmail.com>
2011-10-26 13:04:53 +0300

    Feature #4788: Provide some tests for check singleton pattern on ``setman.Settings`` model.

---
+++ testproject/core/tests/test_models.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+
+from django.test import TestCase
+
+from setman.models import Settings
+
+
+__all__ = ('TestSettings', )
+
+
+TEST_SETTINGS = {
+    'BOOLEAN_SETTING': False,
+    'CHOICE_SETTING': 'pear',
+    'DECIMAL_SETTING': Decimal('8.5'),
+    'INT_SETTING': 24,
+    'FLOAT_SETTING': 80.4,
+    'STRING_SETTING': 'Started with s',
+}
+
+
+class TestSettings(TestCase):
+
+    def test_save(self):
+        settings = Settings()
+
+        for key, value in TEST_SETTINGS.items():
+            setattr(settings.data, key, value)
+
+        settings.save()
+        settings = Settings.objects.get()
+
+        for key, value in TEST_SETTINGS.items():
+            self.assertEqual(getattr(settings.data, key), value)
+
+    def test_unique(self):
+        Settings.objects.create()
+        self.assertRaises(Exception, Settings.objects.create)
