96cd8e331b287447515a88f7e1d8046080976b23
Igor Davydenko <playpauseandstop@gmail.com>
2011-10-28 00:43:51 +0300

    Feature #4791: Refactor test project home page, add fancy styles to test project.

--- testproject/core/views.py
+++
@@ -1,5 +0,0 @@
-from django.views.generic.simple import direct_to_template
-
-
-def homepage(request):
-    return direct_to_template(request, 'homepage.html')
--- testproject/urls.py
+++ testproject/urls.py
@@ -1,6 +1,30 @@
-from django.conf.urls.defaults import patterns, url, include
+from django.conf import settings
+from django.conf.urls.defaults import include, patterns
+from django.contrib import admin
+from django.contrib.auth.decorators import login_required
+from django.views.generic.simple import direct_to_template
+
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'core.views.homepage', name='homepage'),
-    url(r'^setman/', include('setman.urls')),
+    (r'^$', login_required(direct_to_template), {'template': 'home.html'},
+     'home'),
+
+    (r'^odesk_auth/', include('django_odesk.auth.urls')),
+
+    (r'^accounts/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout',
+        {'template_name': 'registration/logout.html'}),
+    (r'^accounts/', include('django.contrib.auth.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/', include(admin.site.urls)),
+
+    (r'^setman/', include('setman.urls')),
 )
+
+if settings.SERVE_STATIC_FILES:
+    urlpatterns += patterns('django.views.static',
+        ('^%s/(?P<path>.*)$' % settings.MEDIA_URL.strip('/'), 'serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
